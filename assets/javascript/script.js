ba3c4ba4e19293c5e1e521036b4bfe4c756c3a21
Rebecca Barrett <52766790+barrett4467@users.noreply.github.com>
2020-09-24 14:55:12 -0600

    Working on contact form. Added PHP but now looking at formspree Github will not allow php hosted on their website.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,25 +1,25 @@
 // console.log("javascript is working");
-var contact = {
-"name": "",
-"email": "",
-"message": ""
-}
+// var contact = {
+// "name": "",
+// "email": "",
+// "message": ""
+// }
 
-function handleSubmit(event) {
-  //prevent refresh
-  event.preventDefault();
+// function handleSubmit(event) {
+//   //prevent refresh
+//   event.preventDefault();
 
-  //grab data from input fields 
-  contact.name = document.getElementById("name").value;
-  contact.email = document.getElementById("email").value;
-  contact.message = document.getElementById("message").value;
+//   //grab data from input fields 
+//   contact.name = document.getElementById("name").value;
+//   contact.email = document.getElementById("email").value;
+//   contact.message = document.getElementById("message").value;
 
-  console.log(`Contact Name: ${contact.name}, ${contact.email}, ${contact.message}`);
+//   console.log(`Contact Name: ${contact.name}, ${contact.email}, ${contact.message}`);
 
 
-  // clear form
-  document.querySelector(".contact-form").reset();
-}
+//   // clear form
+//   document.querySelector(".contact-form").reset();
+// }
 
-//listens for the click on submit button
-document.querySelector(".submit").addEventListener("click", handleSubmit);
+// //listens for the click on submit button
+// document.querySelector(".submit").addEventListener("click", handleSubmit);
